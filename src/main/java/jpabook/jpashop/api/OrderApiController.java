0e76c2a3050698d795713f070a4139bbe8cf38ef
Jealyang <jaelyangchoi@gmail.com>
2024-10-13 14:48:06 +0900

    컬렉션 주문 조회 V1: 엔티티 직접 반환

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(orderItem -> orderItem.getItem().getName());
+        }
+
+        return orders;
+    }
+}
