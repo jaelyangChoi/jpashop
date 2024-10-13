2646c1b314f6d3b7ca527b3a3fffc6eba1e29e4a
Jealyang <jaelyangchoi@gmail.com>
2024-10-13 18:43:45 +0900

    컬렉션 주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -2,12 +2,16 @@ package jpabook.jpashop.repository.order.query;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
     private Long orderId;
     private String name;
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -14,7 +14,8 @@ public class OrderQueryRepository {
 
     private final EntityManager em;
 
-    /** DTO 직접 조회 - 컬렉션 조회
+    /**
+     * DTO 직접 조회 - 컬렉션 조회
      * Query: 루트 1번, 컬렉션 N 번 실행
      * ToOne(N:1, 1:1) 관계들을 먼저 조회하고, ToMany(1:N) 관계는 각각 별도로 처리한다.
      * row 수가 증가하지 않는 ToOne 관계는 조인으로 최적화 하기 쉬우므로 한번에 조회하고,
@@ -29,7 +30,8 @@ public class OrderQueryRepository {
         return orders;
     }
 
-    /** DTO 직접 조회 - 컬렉션 조회 최적화
+    /**
+     * DTO 직접 조회 - 컬렉션 조회 최적화
      * Query: 루트 1번, 컬렉션 1번 (in 쿼리)
      * ToOne 관계들을 먼저 조회하고, 여기서 얻은 식별자 orderId로 ToMany 관계인 OrderItem 을 한꺼번에 조회
      */
@@ -46,6 +48,21 @@ public class OrderQueryRepository {
         result.forEach(o -> o.setOrderItems(orderItemDtoMap.get(o.getOrderId())));
 
         return result;
+    }
+
+    /**
+     * 쿼리는 한번이지만 조인으로 인해 DB에서 애플리케이션에 전달하는 데이터에 중복 데이터가 추가되므로
+     * 상황에 따라 V5 보다 더 느릴 수 도 있다.
+     */
+    public List<OrderFlatDto> findOrderQueryDtos_flat() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 
     private List<OrderItemQueryDto> findOrderItems(List<Long> orderIds) {
@@ -76,6 +93,4 @@ public class OrderQueryRepository {
                                 " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
-
-
 }
